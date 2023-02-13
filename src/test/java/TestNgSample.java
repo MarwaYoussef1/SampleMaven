749ce99f866b3950608e0451bb9989d9d87942c0
MarwaYoussef1 <marwa.abdelrahmaan@gmail.com>
2023-02-13 10:01:05 +0200

    nnn

---
+++ src/test/java/TestNgSample.java
@@ -0,0 +1,11 @@
+import org.testng.annotations.Test;
+
+public class TestNgSample {
+    @Test
+    void setup()
+    {
+        System.out.printf("i am here");
+        System.out.printf("i am here");
+    }
+
+}
